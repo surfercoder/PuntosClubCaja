b41bb8ace0b26f9048781e4908fd777890296e86
Agustín Cassani <agustinscassani@gmail.com>
2025-12-10 07:29:51 -0300

    Puntos Club Caja mobile app cleaned up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import { AuthProvider } from '@/contexts/AuthContext';
-import '@/global.css';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <AuthProvider>
-      <GluestackUIProvider mode="dark">
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="login" />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </GluestackUIProvider>
-    </AuthProvider>
-  );
+  return <Stack />;
 }
